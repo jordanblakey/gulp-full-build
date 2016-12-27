17534b6aba634640975f953c6fb9ac126fd3a28c
Jordan Blakey <jordan@jordanblakey.com>
2016-12-27 09:16:38 -0600

    Initial Commit

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+console.log("Statement One! ");
+console.log("Statement Two! ");
+console.log("Statement Three! ");
+console.log("Statement Four! ");
+
+document.getElementById('sweet-header').innerHTML = 'BODACIOUS HEADER'; 
